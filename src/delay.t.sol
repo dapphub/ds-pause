549d913d837a46a257fa4c44aa79283fdffa4921
David Terry <me@xwvvvvwx.com>
2018-12-28 14:43:06 +0100

    add basic tests for execute

--- src/delay.t.sol
+++ src/delay.t.sol
@@ -4,18 +4,48 @@ import "ds-test/test.sol";
 
 import "./delay.sol";
 
+contract Hevm {
+    function warp(uint256) public;
+}
+
+contract Target {
+    function getBytes32() public pure returns (bytes32) {
+        return bytes32("Hello");
+    }
+}
+
 contract DsDelayTest is DSTest {
     DSDelay delay;
+    Target target;
+    Hevm hevm;
+
+    uint256 start = 1;
+    uint256 wait  = 1;
+    uint256 ready = 3;
 
     function setUp() public {
-        delay = new DSDelay(1);
+        hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+        hevm.warp(start);
+
+        target = new Target();
+        delay = new DSDelay(wait);
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
+    function testFail_execute_delay_not_passed() public {
+        bytes32 id = delay.enqueue(address(target), abi.encode(0));
+        delay.execute(id);
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function test_execute_delay_passed() public {
+        bytes32 id = delay.enqueue(address(target), abi.encodeWithSignature("getBytes32()"));
+        hevm.warp(ready);
+
+        bytes memory response = delay.execute(id);
+
+        bytes32 response32;
+        assembly {
+            response32 := mload(add(response, 32))
+        }
+        assertEq(response32, bytes32("Hello"));
     }
 }

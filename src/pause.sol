ca61d3028b920ff0d1a9f8b9a7b25685ae969331
David Terry <me@xwvvvvwx.com>
2019-04-22 12:35:52 +0200

    Use solidity delegatecall
    Since version 0.5.0, the solidity delegatecall returns the result of the
    call, so the assembly implementation from ds-proxy is no longer needed.

--- src/pause.sol
+++ src/pause.sol
@@ -96,24 +96,11 @@ contract DSPauseProxy {
 
     function exec(address usr, bytes memory fax)
         public
-        returns (bytes memory response)
+        returns (bytes memory out)
     {
         require(msg.sender == owner, "ds-pause-proxy-unauthorized");
-
-        // delegatecall implementation from ds-proxy
-        assembly {
-            let succeeded := delegatecall(sub(gas, 5000), usr, add(fax, 0x20), mload(fax), 0, 0)
-            let size := returndatasize
-
-            response := mload(0x40)
-            mstore(0x40, add(response, and(add(add(size, 0x20), 0x1f), not(0x1f))))
-            mstore(response, size)
-            returndatacopy(add(response, 0x20), 0, size)
-
-            switch iszero(succeeded)
-            case 1 {
-                revert(add(response, 0x20), size)
-            }
-        }
+        bool ok;
+        (ok, out) = usr.delegatecall(fax);
+        require(ok, "ds-pause-delegatecall-error");
     }
 }
